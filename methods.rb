f188dcf74ac89c3a5d90fbda097c60af39d319f8
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-19 21:39:25 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,25 +1,27 @@
 # Your code here!
 # Your code here
 
-def greet_programmer()
+def greet_programmer
     puts "Hello, programmer!"
 end
-   
-   
-def greet (name="Mokua")
+
+def greet name
     puts "Hello, #{name}!"
 end
-   
-def add (a, b)
-    a + b
-end
-   
-def greet_with_default(name="programmer")
+
+def greet_with_default(name = "programmer")
     puts "Hello, #{name}!"
 end
-   
-def halve(a)
-    return nil unless class == Integer
-    a/2
+
+def add (num1, num2)
+    sum = num1 + num2
+    return sum
+end 
+
+puts add(2,5)
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    return num/2
 end
-   
